0c0ee4673b8789b9d5a0435c48f3533621601d56
egargale <enrico.gargale@gmail.com>
2021-12-20 16:34:21 +0100

    incorrect dir

--- services/tests/main.go
+++ services/tests/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"tradier/handler"
-	"github.com/egargale/tradier_gomicro_backend/tests/proto"
+	"github.com/egargale/tradier_gomicro_backend/services/tests/proto"
 
 	"github.com/micro/micro/v3/service"
 	"github.com/micro/micro/v3/service/logger"
