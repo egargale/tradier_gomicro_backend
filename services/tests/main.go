f83185b1abe41bcb2faff10f0e2d832cf5f03ccb
egargale <enrico.gargale@gmail.com>
2021-12-20 16:25:26 +0100

    first test

---
+++ services/tests/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"tradier/handler"
+	pb "tradier/proto"
+
+	"github.com/micro/micro/v3/service"
+	"github.com/micro/micro/v3/service/logger"
+)
+
+func main() {
+	// Create service
+	srv := service.New(
+		service.Name("tradier"),
+		service.Version("latest"),
+	)
+
+	// Register handler
+	pb.RegisterTradierHandler(srv.Server(), handler.New())
+
+	// Run service
+	if err := srv.Run(); err != nil {
+		logger.Fatal(err)
+	}
+}
