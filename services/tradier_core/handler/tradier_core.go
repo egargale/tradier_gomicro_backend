7e24d9364114380ac095e90ebdd480e84586968a
egargale <enrico.gargale@gmail.com>
2021-12-21 08:12:33 +0100

    remove stream ping func

--- services/tradier_core/handler/tradier_core.go
+++ services/tradier_core/handler/tradier_core.go
@@ -16,33 +16,3 @@ func (e *Tradier_core) Call(ctx context.Context, req *tradier_core.Request, rsp 
 	rsp.Msg = "Hello " + req.Name
 	return nil
 }
-
-// Stream is a server side stream handler called via client.Stream or the generated client code
-func (e *Tradier_core) Stream(ctx context.Context, req *tradier_core.StreamingRequest, stream tradier_core.Tradier_core_StreamStream) error {
-	log.Infof("Received Tradier_core.Stream request with count: %d", req.Count)
-
-	for i := 0; i < int(req.Count); i++ {
-		log.Infof("Responding: %d", i)
-		if err := stream.Send(&tradier_core.StreamingResponse{
-			Count: int64(i),
-		}); err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
-// PingPong is a bidirectional stream handler called via client.Stream or the generated client code
-func (e *Tradier_core) PingPong(ctx context.Context, stream tradier_core.Tradier_core_PingPongStream) error {
-	for {
-		req, err := stream.Recv()
-		if err != nil {
-			return err
-		}
-		log.Infof("Got ping %v", req.Stroke)
-		if err := stream.Send(&tradier_core.Pong{Stroke: req.Stroke}); err != nil {
-			return err
-		}
-	}
-}
